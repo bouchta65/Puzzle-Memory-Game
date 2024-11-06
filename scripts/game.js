18bb4fbd9e03ac5e83aef809114803d0e683d8b4
bouchta mohamed <bouchtamohamed01@gmail.com>
2024-11-06 17:48:40 +0100

    Ajout de la fonction d'affichage du popup

--- scripts/game.js
+++ scripts/game.js
@@ -1,30 +1,39 @@
 const cards = document.querySelectorAll(".flip-card");
+
+window.onload = function() {
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].classList.toggle("flip");
+    }
+
+    setTimeout(() => {
+        for(let i = 0; i < cards.length; i++) {
+            cards[i].classList.remove("flip");
+        }
+    }, 600);
+};
+
 let clickedCards = [];
 let wonCards = [];
 
-
 for (let card of cards) {
-    console.log(card);
     card.addEventListener("click", () => displayCard(card));
 }
 
 function displayCard(cardToDisplay) {
-    const clickedBefor = clickedCards.includes(cardToDisplay);
-    const wonBefor = wonCards.includes(cardToDisplay);
-    if (clickedCards.length < 2 && !clickedBefor && !wonBefor) {
+    const clickedBefore = clickedCards.includes(cardToDisplay);
+    const wonBefore = wonCards.includes(cardToDisplay);
+    if (clickedCards.length < 2 && !clickedBefore && !wonBefore) {
         cardToDisplay.classList.toggle("flip");
         clickedCards.push(cardToDisplay);
+        
         if (clickedCards.length === 2) {
-
-            // const firstCardColor = getComputedStyle(clickedCards[0]).backgroundColor;
-            // const secondCardColor = getComputedStyle(clickedCards[1]).backgroundColor;
             const firstID = clickedCards[0].id;
             const secondID = clickedCards[1].id;
 
             setTimeout(() => {
                 if (firstID === secondID) {
-                    wonCards = wonCards.concat(clickedCards)
-                    checkWin(cards);
+                    wonCards = wonCards.concat(clickedCards);
+                    checkWin();
                 } else {
                     for (let clickedCard of clickedCards) {
                         clickedCard.classList.toggle("flip");
@@ -35,13 +44,12 @@ function displayCard(cardToDisplay) {
         }
     }
 }
-function checkWin(allCards) {
-    wonStatus = true;
-    for (let card of allCards) {
-        if (card.classList.contains("flip")) {
-            wonStatus = false;
-            break;
-        }
+
+function checkWin() {
+    if (wonCards.length === cards.length) {
+        document.getElementById("poopup").style.display = "flex";
     }
-    if (wonStatus) { alert("Game Cleared!! You Won !!!"); }
 }
+
+
+
